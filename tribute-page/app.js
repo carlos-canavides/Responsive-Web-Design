8dc28c68de4c3f8cbb9d130d5a7d1059b1537e76
andrea canavides <cjcc1802@gmail.com>
2022-04-26 23:59:42 -0300

    add theme dark and light

--- tribute-page/app.js
+++ tribute-page/app.js
@@ -1,4 +1,5 @@
 const header = document.querySelector("header");
+const toggleBtn = document.querySelector(".toggle-btn");
 
 /* ---------- Stichy Navbar ---------- */
 
@@ -8,4 +9,19 @@ function stickyNavbar() {
 
 stickyNavbar();
 
-window.addEventListener("scroll", stickyNavbar)
+window.addEventListener("scroll", stickyNavbar);
+
+function changeTheme() {
+    if(!document.body.classList.contains("dark")) {
+        document.body.classList.add("dark");
+        toggleBtn.classList.replace("uil-moon", "uil-sun");
+    }
+    else {
+        document.body.classList.remove("dark");
+        toggleBtn.classList.replace("uil-sun", "uil-moon");
+    }
+}
+
+toggleBtn.addEventListener("click", () => {
+    changeTheme()
+});
