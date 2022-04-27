3a09a3a8aad2e6911ec7d1d378e511104f373656
andrea canavides <cjcc1802@gmail.com>
2022-04-27 00:13:46 -0300

    fix dark mode transition

--- tribute-page/app.js
+++ tribute-page/app.js
@@ -11,17 +11,22 @@ stickyNavbar();
 
 window.addEventListener("scroll", stickyNavbar);
 
-function changeTheme() {
-    if(!document.body.classList.contains("dark")) {
+let firstTheme = localStorage.getItem("dark");
+changeTheme(+firstTheme);
+
+function changeTheme(isDark) {
+    if(isDark) {
         document.body.classList.add("dark");
         toggleBtn.classList.replace("uil-moon", "uil-sun");
+        localStorage.setItem("dark", 1);
     }
     else {
         document.body.classList.remove("dark");
         toggleBtn.classList.replace("uil-sun", "uil-moon");
+        localStorage.setItem("dark", 0);
     }
 }
 
 toggleBtn.addEventListener("click", () => {
-    changeTheme()
+    changeTheme(!document.body.classList.contains("dark"))
 });
